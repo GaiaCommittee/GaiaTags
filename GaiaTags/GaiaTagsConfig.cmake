cd591166594d1f65cc1e6a4c4088e2781ee185c7
Vincent <rumble.vecent@outlook.com>
2021-07-22 20:39:13 +0800

    Change install script, add version support.

--- GaiaTags/GaiaTagsConfig.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(3.10)
-
-find_path(GaiaTags_INCLUDE_DIRS "GaiaTags")
-find_library(GaiaTags_LIBS "GaiaTags")
-
-if (GaiaTags_INCLUDE_DIRS AND GaiaTags_LIBS)
-    set(GaiaTags_FOUND TRUE)
-endif()
